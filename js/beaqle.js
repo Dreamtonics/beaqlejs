a3bf04100c4f6074bed96b28d709c94e60ceafd1
Henryzhao <shzhaotc@gmail.com>
2021-12-16 15:47:31 +0800

    Added dynamic test result message.

--- js/beaqle.js
+++ js/beaqle.js
@@ -857,7 +857,7 @@ $.extend({ alert: function (message, title) {
                     dataType: 'json'})
             .done( function (response){
                     if (response.error==false) {
-                        $('#SubmitBox').html("Your submission was successful.<br/><br/>");
+                        $('#SubmitBox').html(response.message+"<br/><br/>");
                         testHandle.TestState.TestIsRunning = 0;
                     } else {
                         $('#SubmitError').show();
